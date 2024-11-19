17bd7724f9bb11895943238ffef3dd6cedf2e443
Samil <samil.moret@gmail.com>
2024-11-19 18:40:25 -0300

    Adicionando o Controller do Login

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -9,7 +9,7 @@ class usuarioController{
             const { email, nome, telefone, password } = req.body;
             const usuarioExiste = await usuariosModel.getOne({ email });
             if (usuarioExiste) {
-                return res.status(400).json({ error: 'O usuário já existe' });
+                return res.status(400).json({ error: 'O usuário está cadastrado' });
             }
 
             const passwordCriptografado = await bcrypt.hash(password, 10);
@@ -27,7 +27,25 @@ class usuarioController{
     }
 
     async login(req, res) {
-        // Implementação futura
+        const {email, password} = req.body;
+
+        const usuarioExiste = await usuariosModel.getOne({ email });
+
+            if (!usuarioExiste) {
+                return res.status(400).json({ error: 'O usuário não cadastrado' });
+        }
+
+        const passwordValido = await bcrypt.compare(password, usuarioExiste.password);
+
+        if (!passwordValido) {
+            return res.status(400).json({ error: 'Password não valido' });
+        }
+
+        return res.status(200).json({msg: 'Usuario Autenticado'})
+
+    }catch (e) {
+       
+        res.status(500).send(e);
     }
 }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,5 +4,6 @@ import usuarioController from '../controllers/usuarios.js';
 const route = express.Router();
 
 route.post('/register', usuarioController.register);
+route.post('/login', usuarioController.login);
 
 export default route;
